141d357ce8b4999b7112f67547af1e5b58aa562d
Noah Ingham <noahingham@gmail.com>
2013-09-21 10:10:43 +1000

    MOre broken than before

--- finalEq.py
+++ finalEq.py
@@ -9,7 +9,7 @@ height = len(iml) #Used to flip
 def fpx(letter): #Returns the top pixels equal to a given letter
     for y in range(height):
         for x in range(len(iml[y])):
-            if iml[y][x]==letter and countSurr(x,y,'4')==0:
+            if iml[y][x]==letter and countSurr(x,y,'4')==0 and countSurr(x,y,'1')>= 1:
                 return (x,y)
     return False
 #Counts number of surrounding pixels equal to a given letter, given an x and y
@@ -23,15 +23,14 @@ def countSurr(x, y, letter):
 
 allPx = []
 # Finds the surrounding pixels
-def surPx(x,y,direction, newN, save, counter=0, inverting=False):
+def surPx(x,y,direction, newN, save, counter=0):
     iml[y][x]= newN #Changes the pixel value.
     if save:
         global allPx
-        allPx.append([x,y])
+        if [x,y] not in allPx:
+            allPx.append([x,y])
         save[0].append(x)
         save[1].append(height-y)
-    if inverting and iml[y+direction[0]][x] not in [' ', '4']:
-        return surPx(x, y+direction[0], direction, newN, save,counter+1)
     if iml[y+direction[0]][x+direction[1]] not in [' ', '4']:
         return surPx(x+direction[1], y+direction[0], direction, newN, save, counter+1)
     if iml[y+direction[0]][x] not in [' ', '4']:
@@ -58,25 +57,19 @@ while True:
             restart=0
         if restart == 2:
             break
-""" Plot
-print('Plotting...')
-xs = []
-ys = []
-for y in range(height):
-    for x in range(len(iml[y])):
-        if iml[y][x] != " ":
-            xs.append(x)
-            ys.append(height-y)
-import matplotlib.pyplot as plt
-plt.plot([xs], [ys], '-bo')
-plt.plot([xrs],[yrs], '-ro')
-plt.show()
-"""
 
-# Day 4
-# The program can spit out line qeuations, given a series of points.
-# Next up is turning an image into a series of points, which can then be plugged into what I have so far.
-# This program uses a Threshold, meaning that the points don't have to be exactly on the line of the points before it, but almost on it.
+def plotPx(pixelList):
+    print('Plotting...')
+    xs = []
+    ys = []
+    for p in pixelList:
+        xs.append(p[0])
+        ys.append(p[1])
+    import matplotlib.pyplot as plt
+    plt.plot([xs], [ys], '-ro')
+    #plt.plot([xrs],[yrs], '-ro')
+    plt.show()
+    print('Done...')
 
 pixels = allPx
 from sympy import *
@@ -107,8 +100,7 @@ def eqLine(listof2):
     else:
         return{'h':x1}, 3
 
-
-def evalPar(point,eqD, line, lineType): # Given an x value and an equation, returns the y-value
+def evalPar(point,eqD, lineType): # Given an x value and an equation, returns the y-value
     x = point[0]
     if lineType==0: # Vertical Parabola
         a,h,k=eqD[Symbol('a')], eqD[Symbol('h')], eqD[Symbol('k')]
@@ -119,49 +111,66 @@ def evalPar(point,eqD, line, lineType): # Given an x value and an equation, retu
     if lineType==2:
         a,h,k=eqD['a'],eqD['h'],eqD['k']
         return a*(x-h) + k
-    if x==eqD['h']:
-        return point[1]
-    return -10000000
+    #if x==eqD['h']:
+    return point[1]
+    #return -10000000
 
 def prettyPar(eqs, lineType): # Prints equations in the classic form.
     if lineType==0:
-        print( '(x-%s)^2 = 4 * %s * (y - %s)' %(eqs[Symbol('h')], eqs[Symbol('a')], eqs[Symbol('k')]))
+        print '(x-%s)^2 = 4 * %s * (y - %s)\t' %(eqs[Symbol('h')], eqs[Symbol('a')], eqs[Symbol('k')]),
     if lineType==1:
-        print( '(y-%s)^2 = 4 * %s * (x - %s)' %(eqs[Symbol('k')], eqs[Symbol('a')], eqs[Symbol('h')]))
+        print '(y-%s)^2 = 4 * %s * (x - %s)\t' %(eqs[Symbol('k')], eqs[Symbol('a')], eqs[Symbol('h')]),
     if lineType==2:
-        print('y=%s(x-%s) + %s' %(eqs['a'], eqs['h'], eqs['k']))
+        print 'y=%s(x-%s) + %s\t\t\t' %(eqs['a'], eqs['h'], eqs['k']),
     if lineType==3:
-        print('x=%s' %(eqs['h']))
+        print 'x=%s\t\t\t\t' %(eqs['h']),
 
 def eqGeneral(listof3):
-    eq = eqVertPar(listof3)
+    (x1,y1),(x2,y2),(x3,y3) = listof3
+    if (x1 == x2 and x2==x3) or (y1==y2 and y2==y3):
+        return eqLine([listof3[0], listof3[2]])
+    eq = eqPar(listof3)
     lineType=0
     if eq == {}:
-        eq = eqVertPar(listof3, False)
+        eq = eqPar(listof3, False)
         lineType=1
     if eq == {}:
         eq,lineType = eqLine([listof3[0], listof3[2]])
     return eq, lineType
 
-notdone,count,lines,thresh,setOfLines=False,3,1,5,[]
+def actualPxList(tempPx, eq, lineType):
+    global actualPx
+    for p in tempPx:
+        try:
+            actualPx.append([p[0],len(iml)-int(evalPar(p, eq, lineType))])
+        except:
+            print("Imaginary")
+
+
+notdone,count,lines,thresh,actualPx,tempPx=False,3,1,[0,0,0,0],[],[pixels[0], pixels[1], pixels[2]]
 eq,lineType = eqGeneral([pixels[0],pixels[1],pixels[2]])
 prettyPar(eq, lineType)
 while True:
     if len(pixels)<=3:
-        print("I'm being used!")
         break
-    if abs(evalPar(pixels[3],eq, line, lineType)-pixels[3][1]) > thresh:
-        print( str(pixels[0]) +  " to " + str(pixels[2]) + "  Pixels: " + str(count) + "\n")
+    if abs(evalPar(pixels[3],eq, lineType)-pixels[3][1]) > thresh[lineType]:
+        #if lineType in [0,1]:
+        tempPx.pop()
+        print( str(pixels[0]) +  " to " + str(pixels[2]) + "  Pixels: " + str(count))
+        actualPxList(tempPx, eq, lineType)
+        lines +=1
         pixels = pixels[3:]
+        tempPx=[pixels[0], pixels[1], pixels[2]]
         if len(pixels) <3:
             notdone=len(pixels)>0
             break
         eq,lineType = eqGeneral([pixels[0],pixels[1],pixels[2]])
+        #if lineType in [0, 1]:
         prettyPar(eq, lineType)
         count = 3
-        lines += 1
         continue
     eq,lineType = eqGeneral([pixels[0],pixels[1],pixels[2]])
+    tempPx.append(pixels[3])
     pixels.pop(1)
     count += 1
 print(pixels[0], " to ", pixels[-1])
@@ -169,3 +178,4 @@ if notdone:
     print(pixels)
     print('Straightline')
 print(lines)
+plotPx(actualPx)
